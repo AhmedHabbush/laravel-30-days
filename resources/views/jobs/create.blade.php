c9a004dc26a6bf4f1b97de689fb2363332d0d8b1
AhmedHabbush <ahabbush@smail.ucas.edu.ps>
2025-12-30 16:27:54 +0200

    Implement manual authentication forms with reusable Blade components

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -12,50 +12,29 @@
                 <p class="mt-1 text-sm/6 text-gray-600">we just need a handful of details from you.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
-                        <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="title" type="text" name="title" placeholder="HR Manager"
-                                       class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
-                            </div>
-                            @error('title')
-                            <p class="text-xs text-red-500 font-semibold">{{$message}}</p>
-                            @enderror
-                        </div>
-                    </div>
 
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                    <x-form-field>
+                        <x-form-label for="title">Title</x-form-label>
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="salary" type="text" name="salary" placeholder="$50,000 per year."
-                                       class="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
-                            </div>
-                            @error('salary')
-                            <p class="text-xs text-red-500 font-semibold">{{$message}}</p>
-                            @enderror
+                            <x-form-input id="title" type="text" name="title" placeholder="CEO"/>
+                            <x-form-error name="title"/>
                         </div>
-                    </div>
+                    </x-form-field>
+
+                    <x-form-field>
+                        <x-form-label for="salary">Salary</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input id="salary" type="text" name="salary" placeholder="$50,000 USD"/>
+                            <x-form-error name="salary"/>
+                        </div>
+                    </x-form-field>
                 </div>
-{{--                @if($errors->any())--}}
-{{--                    <ul>--}}
-{{--                        @foreach($errors->all() as $error)--}}
-{{--                            <li class="text-red-500">{{$error}}</li>--}}
-{{--                        @endforeach--}}
-{{--                    </ul>--}}
-{{--                @endif--}}
             </div>
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-            <button type="submit"
-                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                Save
-            </button>
+            <x-form-button>Save</x-form-button>
         </div>
     </form>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -8,3 +10,11 @@ Route::view('/', 'home');
 Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
+
+//Auth
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
+
+//login
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
